2ebd6c847f60a0b04c38ea5f09ba01e03422ce7f
Hamza Mohammed <hamza.mohammed@keydata.com.tr>
2020-02-21 17:35:56 +0300

    multi label procedures added

--- app.py
+++ app.py
@@ -1,21 +1,29 @@
 from flask import Flask
 
+from FashionPlot import FashionPlot
 from TrainModel import TrainModel
 
 app = Flask(__name__)
 
 train_model = TrainModel()
+fashion = FashionPlot()
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def e_category():
+    return 'ECategory!'
 
 
 @app.route('/train', methods=['GET', 'POST'])
 def train():
     model = train_model.train()
-    return
+    return "model trained successful"
+
+
+@app.route('/fashionPlot', methods=['GET', 'POST'])
+def fashion_plot():
+    fashion_plot = fashion.fashion_plot()
+    return "fashion plotted successful"
 
 
 if __name__ == '__main__':
